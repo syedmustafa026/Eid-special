6410c9be7d77b9ec105746f6d55d45ed59a73092
Syed Mustafa Ahmed <77160234+syedmustafa026@users.noreply.github.com>
2021-06-20 00:24:51 +0500

    Update app.js

--- app.js
+++ app.js
@@ -10,3 +10,4 @@ diff = eid - today
  days = Math.floor(diff /(1000*60*60*24))
 hours = Math.floor((diff % (1000*60*60*24))  / (1000*60*60))
 minutes = Math.floor((diff % (1000*60*60)) / (1000*60));
+seconds = Math.floor((diff % (1000*60))  / 1000)
